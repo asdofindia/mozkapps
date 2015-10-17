0c2082bedcbff3bf1726d443023a34947f931d3d
jigs1212 <jigar.lodaya12@gmail.com>
2015-10-17 23:07:26 +0530

    Added size with size description and the icon of the app

--- js/fetch.js
+++ js/fetch.js
@@ -32,19 +32,45 @@ $( document ).ready(function() {
         xmlHttp.onreadystatechange = handleServerResponse;
         xmlHttp.send(null);
       }
-    
+
 
     function handleServerResponse(){
       if(xmlHttp.readyState == 4){
         if(xmlHttp.status == 200 ){
           var arr = JSON.parse(xmlHttp.responseText);
+          fs = convert (arr.file_size);
           document.getElementById("author").innerHTML = (arr.author);
-          document.getElementById("size").innerHTML = (arr.file_size);
+          document.getElementById("size").innerHTML = (fs).toFixed(2);
+          document.getElementById("icon").innerHTML = "<img src=" + arr.icons[64] + ">";
 
           //document.getElementById("data").innerHTML = (arr.computedString);
         }else {
           alert('something went wrong');
         }
+      }
+    }
+
+    function convert(f) {
+      fkb=(f/1024*100000)/100000;
+      fmb=(f/1048576*100000)/100000;
+      fgb=(f/1073741824*100000)/100000;
+      ftb=(f/1099511627776*100000)/100000;
+
+      if(fgb >= 1){
+        document.getElementById("sizedesc").innerHTML = " GB";
+        return fgb;
+      }
+      else if(fmb >= 1){
+        document.getElementById("sizedesc").innerHTML = " MB";
+        return fmb;
+      }
+      else if(fkb >= 1){
+        document.getElementById("sizedesc").innerHTML = " KB";
+        return fkb;
+      }
+      else{
+        document.getElementById("sizedesc").innerHTML = " Bytes";
+        return f;
       }
     }
 });
